260c8f8f7470754be26c644a304f00b75b45f34c
Uriel Ryan J. Reyles <urielryanreyles@gmail.com>
2023-11-21 22:50:13 +0800

    updated the links in NavBar

--- elementary-app/src/components/NavBar.js
+++ elementary-app/src/components/NavBar.js
@@ -13,10 +13,10 @@ const NavBar = () => {
             <div id="NavBar">
                 <ul>
                     <img src={Logo} alt="Logo" className="Logo"/>
-                    <li><a id="home-navbar" href="../pages/home-page.html">home</a></li>
-                    <li><a id="generate-navbar" href="../pages/custom_button_page.html">generate</a></li>
-                    <li><a id="support-navbar" href="../pages/home-page.html">support</a></li>
-                    <li><a id="about-navbar" href="../pages/home-page.html">about</a></li>
+                    <li><Link to="/home" class="home">home</Link></li>
+                    <li><Link to="/home" class="home">generate</Link></li>
+                    <li><Link to="/home" class="home">support</Link></li>
+                    <li><Link to="/home" class="home">about</Link></li>
                     <li class="right-links">
                         <Link to="/signup">sign up</Link>
                         <Link to="/login" class="signin-button">sign in</Link>
