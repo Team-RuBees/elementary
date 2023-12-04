4cc034c361c81c2fe2ac67cb159054df347500f1
camil <camillegalicia00@gmail.com>
2023-12-04 12:45:12 +0800

    Changed start up page to home page

--- elementary-app/src/App.js
+++ elementary-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index element = {<SignUp/>} />
+          <Route index element = {<Home/>} />
           <Route path="/home" element = {<Home/>} />
           <Route path="/login" element = {<Login/>} />
           <Route path="/signup" element = {<SignUp/>} />
