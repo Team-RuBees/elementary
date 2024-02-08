a8bf4d1602b8ff571166425dd0a2891652f4a643
Aliza <148846971+hsaazill@users.noreply.github.com>
2024-02-08 21:36:41 +0800

    .

--- backend/routers/signup_router.js
+++ backend/routers/signup_router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../models/User");
+const { User } = require("../models/user");
 
 router.post('/', async (req, res) => {
     const { name, email, password } = req.body;
