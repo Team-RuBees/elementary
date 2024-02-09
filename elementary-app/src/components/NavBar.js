884d4682fcf9c39104b09b61093ee2f8e8d2bcd6
Aliza <148846971+hsaazill@users.noreply.github.com>
2024-02-10 01:22:30 +0800

    nav updateee

--- elementary-app/src/components/NavBar.js
+++ elementary-app/src/components/NavBar.js
@@ -6,47 +6,70 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { getCookie, isCookieExpired, removeCookie } from '../utils/cookies';
 
-
 const NavBar = () => {
-    const navigate = useNavigate();
-    const user = getCookie("name")
+  const navigate = useNavigate();
+  const user = getCookie("name");
 
-    const handleLogout = () => {
-        removeCookie("name")
-        navigate("/login")
-    }
-
-    return (
-      <div>
-       
-        <header id="NavBar">
-            <nav>
-                <img src={Logo} alt="Logo" className="Logo"/>
-
-                    <h4 for="toggler">
-                        <FontAwesomeIcon icon={faBars} />
-                    </h4>
-                <input type="checkbox" id="toggler" name=""/>
-                
-                <div class="menu">
-                    <ul class="list">
-                        <li><Link to="/home" class="btn">home</Link></li>
-                        <li><Link to="/button" class="btn">generate</Link></li>
-                        <li><Link to="/help" class="btn">support</Link></li>
-                        <li><Link to="/about" class="btn">about</Link></li>
-                        
-                        <li><Link to="/signup" class="signup">sign up</Link></li>
-                        <li><Link to="/login" class="login">sign in</Link></li>
-                    </ul>
-                </div>
-            </nav>        
-        </header>
-    
-      </div>
-      
-    );
+  const handleLogout = () => {
+    removeCookie("name");
+    navigate("/login");
   };
-  
-  export default NavBar;
 
+  return (
+    <div>
+      <header id="NavBar">
+        <nav>
+          <img src={Logo} alt="Logo" className="Logo" />
+          <h4 for="toggler">
+            <FontAwesomeIcon icon={faBars} />
+          </h4>
+          <input type="checkbox" id="toggler" name="" />
+          <div className="menu">
+            <ul className="list">
+              <li>
+                <Link to="/home" className="btn">home</Link>
+              </li>
+              <li>
+                <Link to="/button" className="btn"> generate</Link>
+              </li>
+              <li>
+                <Link to="/help" className="btn"> support</Link>
+              </li>
+              <li>
+                <Link to="/about" className="btn"> about</Link>
+              </li>
+              {user ? (
+                <li className="nav-right-container right-links">
+                  <li>
+                    <Link>
+                      <p>{user}</p> </Link>
+                  </li>
+                  <li>
+                    <button
+                      className="signin-button" onClick={handleLogout}
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <Link to="/signup" className="signup"> sign up </Link>
+                  </li>
+                  <li>
+                    <Link to="/login" className="login">
+                      sign in
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
 
+export default NavBar;
